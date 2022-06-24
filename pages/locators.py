26848372f4fcb0e104422977386b60b860aa12df
Tanya <skkts31@gmail.com>
2022-06-24 20:56:14 +0500

    added asserts and locators for login page

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '[name="login_submit"]')
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,3 +13,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_and_register_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
