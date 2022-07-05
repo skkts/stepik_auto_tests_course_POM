91caf6f9217f1ca63bf8d69a108e3fb067cdc154
Tanya <skkts31@gmail.com>
2022-07-05 23:57:39 +0500

    put the structure in order

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,6 @@ import faker
 f = faker.Faker()
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
-@pytest.mark.new
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -17,6 +16,7 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(f.email(), "jsbldi21638dhG")
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         page = ProductPage(browser, link)
         page.open()
@@ -33,6 +33,7 @@ base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
 urls = [f"{base_link}?promo=offer{n}" if n != 7
         else pytest.param("bugged_link", marks=pytest.mark.xfail) for n in range(10)]
 
+@pytest.mark.need_review
 @pytest.mark.parametrize("link", urls)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -66,6 +67,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -73,6 +75,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
